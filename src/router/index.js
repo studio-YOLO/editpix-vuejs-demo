c29fd67963e6d31f568f592d7669c3b89915013e
DPende <pendesini.daniele@gmail.com>
2024-04-20 17:35:12 +0200

    chore: changed path

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import GrayScaleDemo from '../views/GrayScaleDemo.vue'
+import ChangeTemperatureDemo from '../views/ChangeTemperatureDemo.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/change_temperature',
+      name: 'change_temperature',
+      component: ChangeTemperatureDemo
+    },
+    {
+      path: '/gray_scale',
+      name: 'gray_scale',
+      component: GrayScaleDemo
+    }
+  ]
+})
+
+export default router
