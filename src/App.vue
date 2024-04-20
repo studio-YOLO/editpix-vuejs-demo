5725c9d1545152bf9ef90e9066556cac5e284ba3
DPende <pendesini.daniele@gmail.com>
2024-04-20 17:34:27 +0200

    add: added header

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from "@/components/Header.vue"
+import Footer from "@/components/Footer.vue"
+</script>
+
+<template>
+  <v-app>
+    
+      <Header />
+      <v-main>
+      <RouterView />
+    </v-main>
+    <Footer />
+  </v-app>
+
+
+</template>
+
+<style scoped></style>
